e06580dda65b3047f79a6d4a51b3ce9e61d79514
Delmar-Ty <reliablesource13@gmail.com>
2022-03-03 18:25:02 -0700

    This

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,1 +1,9 @@
-//JavaScript
+var imgIDArray = ['product-image', 'product-image-2', 'product-image-3', 'product-image-4'];
+function changeImage() {
+    var img = this.getAttribute('src');
+    document.getElementById('product-image-main').setAttribute('src', img);
+}
+
+for (i = 0; i < imgIDArray.length; i++) {
+    document.getElementById(imgIDArray[i]).addEventListener('click', changeImage);
+}
